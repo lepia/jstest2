ec1fb7a9a3b1922ce70927d7e1ee79fb7f4415e0
lepia <myfigi@naver.com>
2018-11-20 14:54:49 +0900

    오브젝트 생성

---
+++ Sample01/object.js
@@ -0,0 +1,11 @@
+
+
+var obj = {}; // 빈 객체 생성
+// var obj = new Object(); 빈 객체 생성2
+
+obj.name = "Young";
+console.log(obj.name)
+var obj2 = new Object();
+obj2["name"] = "홍길동";
+console.log(obj2);
+
